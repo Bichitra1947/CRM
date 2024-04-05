04eeb43726074f4d63ee5378ab6d13440a84f53c
Bichitra1947 <154616919+Bichitra1947@users.noreply.github.com>
2024-04-05 21:30:36 +0530

    Delete src directory

--- src/main/java/in/sp/main/urls/OtherUrls.java
+++
@@ -1,7 +0,0 @@
-package in.sp.main.urls;
-
-public class OtherUrls 
-{
-	public static String IMAGE_UPLOAD_FOLDER = "src/main/resources/static/uploads";
-	public static String IMAGE_UPLOAD_URL = "http://localhost:8080/uploads/";
-}
